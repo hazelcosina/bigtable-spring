f77411f51eed1ffeb1bc8c4dbcd24a98bbd98373
Hazel Cosina <hazelcosina@gmail.com>
2021-10-22 07:46:27 +0800

    updates

--- src/main/java/com/example/demo/config/TWebClient.java
+++ src/main/java/com/example/demo/config/TWebClient.java
@@ -3,18 +3,27 @@ package com.example.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.transport.ProxyProvider;
 
 @Configuration
 public class TWebClient {
 
     @Bean
     WebClient webClient() {
+
+        HttpClient httpClient = HttpClient.create()
+                .tcpConfiguration(tcpClient ->
+                        tcpClient.proxy(proxy -> proxy.type(ProxyProvider.Proxy.HTTP).host("myproxyhost")));
+        ReactorClientHttpConnector connector = new ReactorClientHttpConnector(httpClient);
+
         return WebClient.builder()
+                .clientConnector(connector)
                 .baseUrl("http://localhost:8080")
                 .defaultHeaders(httpHeaders -> {
                     httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-
                 })
                 .build();
     }
